124dad05a05fb8755a50c1c6bac62a96e4774e8d
Walter Zhang <walter.zhangfang@gmail.com>
2023-11-23 09:52:23 +1300

    Update steps.java

--- 05-Automation-Testing/03-Cucumber/opencart_cucumber_framework/src/test/java/stepDefinitions/steps.java
+++ 05-Automation-Testing/03-Cucumber/opencart_cucumber_framework/src/test/java/stepDefinitions/steps.java
@@ -27,53 +27,47 @@ import pageObjects.MyAccountPage;
 import utilities.DataReader;
 
 public class steps {
-     WebDriver driver;
-     HomePage hp;
-     LoginPage lp;
-     MyAccountPage macc;
+    WebDriver driver;
+    HomePage hp;
+    LoginPage lp;
+    MyAccountPage macc;
 
-     List<HashMap<String, String>> datamap; //Data driven
+    List<HashMap<String, String>> datamap; // Data driven
 
-     public Logger logger; //for logging
-     public ResourceBundle rb; // for reading properties file
-     public String br; //to store browser name
-
-
+    public Logger logger; // for logging
+    public ResourceBundle rb; // for reading properties file
+    public String br; // to store browser name
 
     @Before
-    public void setup()    //Junit hook - executes once before starting
+    public void setup() // Junit hook - executes once before starting
     {
-        //for logging
-        logger= LogManager.getLogger(this.getClass());
-        //Reading config.properties (for browser)
-        rb=ResourceBundle.getBundle("config");
-        br=rb.getString("browser");
-      
-        macc=new MyAccountPage(driver);
-                
+        // for logging
+        logger = LogManager.getLogger(this.getClass());
+        // Reading config.properties (for browser)
+        rb = ResourceBundle.getBundle("config");
+        br = rb.getString("browser");
+
+        macc = new MyAccountPage(driver);
 
     }
 
     @After
     public void tearDown(Scenario scenario) {
-        System.out.println("Scenario status ======>"+scenario.getStatus());
-        if(scenario.isFailed()) {
-             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png",scenario.getName());
-            }
-       driver.quit();
+        System.out.println("Scenario status ======>" + scenario.getStatus());
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        driver.quit();
     }
 
     @Given("User Launch browser")
     public void user_launch_browser() {
-        if(br.equals("chrome"))
-        {
-           driver=new ChromeDriver();
-        }
-        else if (br.equals("firefox")) {
+        if (br.equals("chrome")) {
+            driver = new ChromeDriver();
+        } else if (br.equals("firefox")) {
             driver = new FirefoxDriver();
-        }
-        else if (br.equals("edge")) {
+        } else if (br.equals("edge")) {
             driver = new EdgeDriver();
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -87,10 +81,10 @@ public class steps {
 
     @When("User navigate to MyAccount menu")
     public void user_navigate_to_my_account() {
-    	hp=new HomePage(driver);
-    	hp.clickMyAccount();
+        hp = new HomePage(driver);
+        hp.clickMyAccount();
         logger.info("Clicked on My Account ");
-            
+
     }
 
     @When("click on Login")
@@ -101,9 +95,9 @@ public class steps {
 
     @When("User enters Email as {string} and Password as {string}")
     public void user_enters_email_as_and_password_as(String email, String pwd) {
-    	lp=new LoginPage(driver);
-         
-    	lp.setEmail(email);
+        lp = new LoginPage(driver);
+
+        lp.setEmail(email);
         logger.info("Provided Email ");
         lp.setPassword(pwd);
         logger.info("Provided Password ");
@@ -115,84 +109,65 @@ public class steps {
         logger.info("Clicked on Login button");
     }
 
-
     @Then("User navigates to MyAccount Page")
     public void user_navigates_to_my_account_page() {
-    	macc=new MyAccountPage(driver);
-		boolean targetpage=macc.isMyAccountPageExists();
-	
-        if(targetpage)
-        {
+        macc = new MyAccountPage(driver);
+        boolean targetpage = macc.isMyAccountPageExists();
+
+        if (targetpage) {
             logger.info("Login Success ");
             Assert.assertTrue(true);
-        }
-        else
-        {
+        } else {
             logger.error("Login Failed ");
             Assert.assertTrue(false);
         }
 
     }
 
-    //*******   Data Driven test method    **************
+    // ******* Data Driven test method **************
     @Then("check User navigates to MyAccount Page by passing Email and Password with excel row {string}")
-    public void check_user_navigates_to_my_account_page_by_passing_email_and_password_with_excel_data(String rows)
-    {
-        datamap=DataReader.data(System.getProperty("user.dir")+"\\testData\\Opencart_LoginData.xlsx", "Sheet1");
+    public void check_user_navigates_to_my_account_page_by_passing_email_and_password_with_excel_data(String rows) {
+        datamap = DataReader.data(System.getProperty("user.dir") + "\\testData\\Opencart_LoginData.xlsx", "Sheet1");
 
-        int index=Integer.parseInt(rows)-1;
-        String email= datamap.get(index).get("username");
-        String pwd= datamap.get(index).get("password");
-        String exp_res= datamap.get(index).get("res");
+        int index = Integer.parseInt(rows) - 1;
+        String email = datamap.get(index).get("username");
+        String pwd = datamap.get(index).get("password");
+        String exp_res = datamap.get(index).get("res");
 
-        lp=new LoginPage(driver);
+        lp = new LoginPage(driver);
         lp.setEmail(email);
         lp.setPassword(pwd);
 
         lp.clickLogin();
-        try
-        {
-            boolean targetpage=macc.isMyAccountPageExists();
+        try {
+            boolean targetpage = macc.isMyAccountPageExists();
 
-            if(exp_res.equals("Valid"))
-            {
-                if(targetpage==true)
-                {
-                    MyAccountPage myaccpage=new MyAccountPage(driver);
+            if (exp_res.equals("Valid")) {
+                if (targetpage == true) {
+                    MyAccountPage myaccpage = new MyAccountPage(driver);
                     myaccpage.clickLogout();
                     Assert.assertTrue(true);
-                }
-                else
-                {
+                } else {
                     Assert.assertTrue(false);
                 }
             }
 
-            if(exp_res.equals("Invalid"))
-            {
-                if(targetpage==true)
-                {
+            if (exp_res.equals("Invalid")) {
+                if (targetpage == true) {
                     macc.clickLogout();
                     Assert.assertTrue(false);
-                }
-                else
-                {
+                } else {
                     Assert.assertTrue(true);
                 }
             }
 
-
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
 
             Assert.assertTrue(false);
         }
         driver.close();
     }
 
-    //*******   Account Registration Methods    **************
-
-   
+    // ******* Account Registration Methods **************
 
 }
